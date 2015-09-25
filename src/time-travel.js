30049fd57b020470ac317e64952409d9f3320092
Bob van der Linden <bobvanderlinden@gmail.com>
2015-09-25 14:53:20 +0200

    time-travel: added tests and fixed several implementation errors

--- src/time-travel.js
+++ src/time-travel.js
@@ -1,65 +1,112 @@
 import Cycle from '@cycle/core';
 import TimedArray from './timedarray';
 
-function assert(b) {
+function assert(b, message) {
   if (!b) {
-    throw new Error('Assertion failed');
+    throw new Error('Assertion failed: ' + message);
   }
 }
 
-function makeTimeTravelDriver() {
+function assertEvent(event) {
+  assert(event);
+  assert('type' in event, 'No type in event: ' + JSON.stringify(event));
+  assert('time' in event, 'No time in event: ' + JSON.stringify(event));
+  assert(typeof event.time === 'number');
+  assert(typeof event.type === 'string');
+  assert(['state', 'action', 'time'].indexOf(event.type) >= 0);
+  switch(event.type) {
+    case 'state':
+      assert('state' in event);
+      break;
+    case 'action':
+      assert('action' in event);
+      break;
+    case 'time':
+      break;
+    default: assert(false);
+  }
+}
+
+function makeTimeTravelDriver(options) {
+  function debug(...args) {
+    if (options.debug) {
+      console.log.apply(console, args);
+    }
+  }
   return function timeTravelDriver(events$) {
     var timedStates = new TimedArray();
     var timedActions = new TimedArray();
     var lastTime = 0;
 
     var outputEvents = events$
+      .do(debug.bind(null, '>'))
+      .do(event => assertEvent(event))
       .flatMap(function(event) {
+        var result;
         switch(event.type) {
-          case 'time': return handleTime(event);
-          case 'state': return handleState(event);
-          case 'action': return handleAction(event);
-          default: throw (event.type + " not supported");
+          case 'time': result = handleTime(event); break;
+          case 'state': result = handleState(event); break;
+          case 'action': result = handleAction(event); break;
+          default: throw ('Type ' + event.type + " not supported");
         }
+        return result;
       })
-      .do(event => {
-        assert('type' in event);
-        assert('time' in event);
-        assert(typeof event.time === 'number');
-        assert(typeof event.type === 'string');
-        switch(event.type) {
-          case 'state':
-            assert('state' in event);
-            break;
-          case 'action':
-            assert('action' in event);
-            break;
-          case 'time':
-            break;
-          default: assert(false);
-        }
-      })
+      .do(debug.bind(null, '<'))
+      .do(event => assertEvent(event))
       .share();
 
     function handleTime(event) {
       var newTime = event.time;
-
+      debug('@', lastTime, 'handleTime', {time:event.time});
       if (newTime > lastTime) {
         // Traveling forward in time.
-        // We need to output all passed actions.
-        // We do not have to output any states, because this will result from the actions being handled.
-        var passedTimedActions = timedActions.sliceAtUntil(lastTime, newTime);
+        // We need to output the state before the new time proceeded by
+        // all actions that happened after that state (but before the new time).
+        let timedState = timedStates.getAtOrBeforeTime(newTime);
+        debug('timedState before ', newTime, ':', timedState);
 
-        lastTime = newTime;
-        return Cycle.Rx.Observable.fromArray(passedTimedActions).map(timedAction => ({ type: 'action', action: timedAction.action, time: timedAction.time }));
+        // If no state was found, then there are no states before the new time.
+        // This is kind of an edge-case, because usually we do have a state at time 0.
+        // We will output nothing.
+        if (!timedState) {
+          lastTime = newTime;
+          debug('No state found before', newTime);
+          return Cycle.Rx.Observable.empty();
+        }
+
+        // Retrieve the actions that happened after the timedState, but before our target time.
+        var passedTimedActions = timedActions.sliceInterval({
+          includeFirst: false,
+          includeLast: true,
+          first: timedState.time,
+          last: newTime
+        });
+
+        let stateEvents = Cycle.Rx.Observable.of({type: 'state', time: timedState.time, state: timedState.state});
+        let actionEvents = Cycle.Rx.Observable.fromArray(passedTimedActions)
+            .take(1) // Only output the first action, as its state will be recalculated and send back.
+            .map(timedAction => ({ type: 'action', time: timedAction.time, action: timedAction.action }));
+
+        if (timedState.time <= lastTime) {
+          // If the state was created before our travel started,
+          // we don't have to output that state, since it is still in place.
+          lastTime = newTime;
+          return actionEvents;
+        } else {
+          return Cycle.Rx.Observable.concat(
+            stateEvents,
+            actionEvents
+          );
+        }
       } else if (newTime < lastTime) {
         // Traveling backwards in time.
         // We only need to output the state of the past.
         // We do not have to output any actions, because the actions between the past and now in the past.
-        timedState = timedStates.getAtOrBeforeTime(newTime);
+        let timedState = timedStates.getAtOrBeforeTime(newTime);
 
         lastTime = newTime;
-        return Cycle.Rx.Observable.of(timedStates[newStateIndex]).map(timedState => ({ type: 'state', state: timedState.state, time: timedState.time }));
+        return Cycle.Rx.Observable.of(timedState)
+          .map(timedState => ({ type: 'state', time: timedState.time, state: timedState.state }));
       } else if (newTime === lastTime) {
         // No change in time. Nothing happens.
         return Cycle.Rx.Observable.empty();
@@ -67,50 +114,68 @@ function makeTimeTravelDriver() {
     }
 
     function handleState({state, time}) {
+      debug('@', lastTime, 'handleState', {time, state});
+      assert(typeof time === 'number');
       if (time > lastTime) {
         // State was created in the future.
-        // Our future actions should give us that same future state, so
-        // we may forget about this future state.
+        // Our current state with future actions should give us that same future state,
+        // however to save us from recalculating this state, we will save it now.
+        timedStates.insert({ time: time, state: state });
         return Cycle.Rx.Observable.empty();
       } else if (time < lastTime) {
         // State was created in the past.
         // We have to forget all succeeding states and regenerate states from the past
         // with the actions that occurred after this created state.
         timedStates.removeAtOrAfter(time);
-        timedStates.insert({ state: state, time: time });
-        var actions = timedActions.sliceAtUntil(time, lastTime);
-        return Cycle.Rx.Observable.concat(
-          Cycle.Rx.Observable.of({ type: 'state', state: state, time: time }),
-          Cycle.Rx.Observable.fromArray(actions)
-        );
+        timedStates.insert({ time: time, state: state });
+        var pendingActions = timedActions.sliceInterval({
+          includeFirst: false, includeLast: true,
+          first: time,
+          last: lastTime
+        });
+        debug(pendingActions.length, 'actions to process');
+        if (pendingActions.length === 0) {
+          return Cycle.Rx.Observable.of({ type: 'state', time: time, state: state })
+        } else {
+          var nextPendingAction = pendingActions[0];
+          return Cycle.Rx.Observable.concat(
+            Cycle.Rx.Observable.of({ type: 'state', time: time, state: state }),
+            Cycle.Rx.Observable.just({ type: 'action', time: nextPendingAction.time, action: nextPendingAction.action })
+          );
+        }
       } else if (time === lastTime) {
         // New state was created for our current time.
         // Output it like usual.
-        return Cycle.Rx.Observable.of({ type: 'state', state: state, time: time });
+        timedStates.insert({ state: state, time: time });
+        return Cycle.Rx.Observable.of({ type: 'state', time: time, state: state });
+      } else {
+        throw "This cannot happen";
       }
     }
 
     function handleAction({action, time}) {
-      if (time >= lastTime) {
+      debug('@', lastTime, 'handleAction', {time,action});
+      if (time > lastTime) {
         // Action occured now or in the future.
         // Future actions do not have impact on our current state.
         // Just insert the action for future use.
 
-        // TODO: Maybe actions 'now' should be handled directly.
-        
-        timedActions.insert({ action: action, time: time });
+        timedActions.insert({ time: time, action: action });
         return Cycle.Rx.Observable.empty();
       } else if (time < lastTime) {
         // Action occured in the past.
         // We need to recreate our current state with the past action mixed in.
-        timedActions.insert({ action: action, time: time });
+        timedActions.insert({ time: time, action: action });
         var pastTimedState = timedStates.getAtOrBeforeTime(time);
         var pastTimedActions = timedActions.sliceAtUntil(time, lastTime)
-          .map(timedAction => Object.assign({ type: 'action' }, timedAction));
+          .map(timedAction => ({ type: 'action', time: timedAction.time, action: timedAction.action }));
         return Cycle.Rx.Observable.concat(
-          Cycle.Rx.Observable.of(Object.assign({ type: 'state' }, pastTimedState)),
+          Cycle.Rx.Observable.of({ type: 'state', time: pastTimedState.time, state: pastTimedState.state }),
           Cycle.Rx.Observable.fromArray(pastTimedActions)
         );
+      } else {
+        timedActions.insert({ time: time, action: action });
+        return Cycle.Rx.Observable.of({ type: 'action', time: time, action: action });
       }
     }
 
--- test/time-travel.js
+++ test/time-travel.js
@@ -2,6 +2,8 @@ import Cycle from '@cycle/core';
 import {makeTimeTravelDriver} from '../src/time-travel';
 var assert = require('assert');
 
+Cycle.Rx.config.longStackSupport = true;
+
 var Rx = Cycle.Rx;
 var onNext = Rx.ReactiveTest.onNext;
 var onCompleted = Rx.ReactiveTest.onCompleted;
@@ -10,16 +12,16 @@ var subscribe = Rx.ReactiveTest.subscribe;
 function state(time, value) {
   return {
     type: 'state',
-    state: value,
-    time: time
+    time: time,
+    state: value
   };
 }
 
 function action(time, value) {
   return {
     type: 'action',
-    action: value,
-    time: time
+    time: time,
+    action: value
   };
 }
 
@@ -30,32 +32,220 @@ function time(time) {
   };
 }
 
-var sequenceA = [
-  state(1, 0),
-  action(1, 1),
-  action(2, 1),
-  action(3, 1)
-];
+function rxDeepEqual(a,b) {
+  function toJson(notification) {
+    switch(notification.value.kind) {
+      case 'N':
+        return {
+          kind: 'N',
+          time: notification.time,
+          value: notification.value.value
+        };
+      case 'E':
+        console.log(notification.value.exception.stack);
+        return {
+          kind: 'E',
+          time: notification.time,
+          exception: notification.value.exception
+        };
+      case 'C':
+        return {
+          kind: 'C',
+          time: notification.time
+        };
+      default:
+        throw new Error('Should not happen');
+    }
+  }
+  assert.deepEqual(a.map(toJson), b.map(toJson));
+}
 
-var scheduler = new Cycle.Rx.TestScheduler();
+function testTimeTravel(options, notifications, expected) {
+    var scheduler = new Cycle.Rx.TestScheduler();
+    var inputEvents = scheduler.createHotObservable(notifications);
+
+    var res = scheduler.startWithTiming(function () {
+      let {outputEvents} = makeTimeTravelDriver(options)(inputEvents);
+      return outputEvents;
+    }, 0, 0, 1000);
+
+    rxDeepEqual(res.messages, expected);
+}
 
 describe('TimeTravel', function() {
+  it('does not output time events', function() {
+    testTimeTravel({},[
+      onNext(30, time(100))
+    ], []);
+  });
+
+  it('outputs state that has just been created', function() {
+    testTimeTravel({}, [
+      onNext(10, time(100)),
+      onNext(20, state(100, 1))
+    ], [
+      onNext(20, state(100, 1))
+    ]);
+  });
+
+  it('outputs state that was created in the past', function() {
+    testTimeTravel({}, [
+      onNext(10, time(100)),
+      onNext(20, state(0, 1))
+    ], [
+      onNext(20, state(0, 1))
+    ]);
+  });
+
+  // TODO: This test is not realistic: states created in future are strange.
+  it('outputs latest state when traveling to future', function() {
+    testTimeTravel({}, [
+      onNext(40, time(0)),
+      onNext(30, state(10, 1)),
+      onNext(40, time(100))
+    ], [
+      onNext(40, state(10, 1))
+    ]);
+  });
+
+  // TODO: This test is not realistic: states created in future are strange.
+  it('outputs state at exact time when traveling to future', function() {
+    testTimeTravel({}, [
+      onNext(40, time(0)),
+      onNext(30, state(10, 1)),
+      onNext(40, time(10))
+    ], [
+      onNext(40, state(10, 1))
+    ]);
+  });
+
+  it('outputs latest state and passed actions when traveling to future', function() {
+    testTimeTravel({}, [
+      onNext(20, state(0, 1)),
+      onNext(30, action(1, 1)),
+      onNext(40, action(2, 2)),
+      onNext(50, time(100))
+    ], [
+      onNext(20, state(0, 1)),
+      onNext(50, action(1, 1)),
+    ]);
+  });
+
+  it('outputs latest state and passed actions when traveling to future', function() {
+    testTimeTravel({}, [
+      onNext(20, state(0, 1)),
+      onNext(30, action(1, 1)),
+      onNext(40, action(2, 2)),
+      onNext(50, time(100))
+    ], [
+      onNext(20, state(0, 1)),
+      onNext(50, action(1, 1)),
+    ]);
+  });
+
+  it('should be able to jump through time', function() {
+    testTimeTravel({}, [
+      // Building the actions/states
+      onNext(20, state(0, 1)),
+      onNext(30, action(10, 1)),
+      onNext(40, action(20, 2)),
+      onNext(50, time(100)),
+      onNext(51, state(10, 2)),
+      onNext(52, state(20, 4)),
+
+      // Jumping through time.
+      onNext(60, time(15)),
+      onNext(70, time(10)),
+      onNext(80, time(100)),
+    ], [
+      onNext(20, state(0, 1)),
+      onNext(50, action(10, 1)),
+      onNext(51, state(10, 2)),
+      onNext(51, action(20, 2)),
+      onNext(52, state(20, 4)),
+      onNext(60, state(10, 2)),
+      onNext(70, state(10, 2)),
+      onNext(80, state(20, 4))
+    ]);
+  });
+
+  it('normal non-time-traveling', function() {
+    testTimeTravel({}, [
+      onNext(20, state(0, 1)),
+      onNext(110, time(1)),
+      onNext(112, action(1, 1)),
+      onNext(113, state(1, 2)),
+      onNext(120, time(2)),
+      onNext(130, time(3)),
+      onNext(138, action(3, 1)),
+      onNext(139, state(3, 3)),
+      onNext(140, time(4)),
+      onNext(150, time(5)),
+      onNext(151, action(5, 1)),
+      onNext(152, state(5, 4)),
+      onNext(160, time(6)),
+      onNext(190, time(100))
+    ], [
+      onNext(20, state(0, 1)),
+      onNext(112, action(1, 1)),
+      onNext(113, state(1, 2)),
+      onNext(138, action(3, 1)),
+      onNext(139, state(3, 3)),
+      onNext(151, action(5, 1)),
+      onNext(152, state(5, 4))
+    ]);
+  });
+
+
   it('#fsddf', function() {
-    var i =0;
+    return;
+    function updateState({state}, {action, time}) {
+      return state + action;
+    }
+
+    var initialState = 0;
+
+    var subject = new Rx.BehaviorSubject(state(0, initialState));
+
     var events$ = scheduler.createColdObservable(
-      onNext(0, time(0)),
+      onNext(20, time(11)),
       onNext(10, state(0, 0)),
-      onNext(20, action(10, 1)),
-      onNext(50, time(5))
-    );
+      onNext(20, action(10, 1))
+    ).merge(subject)
+    .do(event => console.log('> event', JSON.stringify(event)));
 
     let {outputEvents} = makeTimeTravelDriver()(events$);
 
+    outputEvents = outputEvents
+      .do(event => console.log('< event', JSON.stringify(event)))
+      .share();
+
+    let outputActions = outputEvents
+      .filter(event => event.type === 'action');
+    let outputStates = outputEvents
+      .filter(event => event.type === 'state');
+
+    Cycle.Rx.Observable.combineLatest([outputStates, outputActions], function({stateTime,state}, {time,action}) {
+      assert(stateTime <= time);
+
+      let newState = updateState(state, action);
+      return {
+        type: 'state',
+        state: newState,
+        time: time
+      };
+    })
+    .do(state => console.log('state', state))
+    .subscribe(subject);
+
     var res = scheduler.startWithCreate(function () {
-      return outputEvents;
+      return outputEvents.merge(subject);
     });
 
-    console.log(res.messages.map(message => message.value.value));
-    assert.equal(1, 2);
+    res.messages.filter(message => message.value.kind === 'E').forEach(message => console.log('message', message.value.exception.stack));
+
+    res.messages.map(message => message.value).forEach(message => console.log(message.kind, message.value));
+
+    assert.equal(res.messages, []);
   });
 });
